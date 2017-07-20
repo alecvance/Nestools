88e6917dedd62718a5839234355ed933f7abda5f
Alec Vance <alec.vance@gmail.com>
2017-07-19 23:04:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+This script is a template to start working with the Nest API and log data from your thermostats.
+
+It does not do any authentication; you will have to do that manually in order to get a token. 
+The steps are generally outlined below. For specifics, see the Nest Developer site.
+
+To get an access token:
+
+1) Get a Nest Developer account.
+2) Create and register a Nest app from your Nest Developer account and set privileges on your Nest app. 
+3) Use OAUTH to setup and authorize your Nest device with the code.
+4) Get the access token for your Nest account to connect to the app.
+
+Once you have a token:
+
+5) Rename sample.config to nest.config and put your access token in there.
+6) Install Python 3 if necessary.
+7) Run the script from a terminal: "python3 nest.py", or add to your crontab to run periodically. 
+
+Each run will append to log file(s); there is a different log file for each thermostat.
+
+The log files show the following per line in order:
+	datetime, inside temperature, inside relative humidity, current state, target temperature.
+
---
+++ nest.py
@@ -0,0 +1,60 @@
+import http.client
+from urllib.parse import urlparse
+import json
+import time
+from datetime import datetime
+import configparser
+
+start_time = time.clock()
+
+config = configparser.ConfigParser()   
+config.read("nest.config")
+
+token = config['DEFAULT']['token']
+
+conn = http.client.HTTPSConnection("developer-api.nest.com")
+headers = {'authorization': "Bearer {0}".format(token)}
+conn.request("GET", "/", headers=headers)
+response = conn.getresponse()
+
+if response.status == 307:
+    redirectLocation = urlparse(response.getheader("location"))
+    conn = http.client.HTTPSConnection(redirectLocation.netloc)
+    conn.request("GET", "/", headers=headers)
+    response = conn.getresponse()
+    if response.status != 200:
+        raise Exception("Redirect with non 200 response")
+
+data = response.read()
+#print(data.decode("utf-8"))
+parsed_json = json.loads(data)
+
+devices = parsed_json['devices']
+thermostats = devices['thermostats']
+
+for deviceID, thermostat in thermostats.items():
+
+	device_name_long = thermostat['name_long']
+	filename = device_name_long + ".log"
+
+	#print(deviceID, 'corresponds to', device_name_long)
+	#for thermostatDataKey, thermostatData in thermostat.items():
+		#print(device_name_long, " ", thermostatDataKey, ': ', thermostatData)
+
+	humidity = thermostat['humidity']
+	hvac_state = thermostat['hvac_state']
+	hvac_mode = thermostat['hvac_mode']
+	target_temperature_f = thermostat['target_temperature_f']
+	ambient_temperature_f = thermostat['ambient_temperature_f']
+
+	timeStr = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+	with open(filename, "a") as f:
+		f.write('{}\t{}\t{}\t{}\t{}\r'.format(timeStr,ambient_temperature_f,humidity,hvac_state,target_temperature_f))
+
+
+print("run time:", time.clock() - start_time, "sec")
+
+
+
+
