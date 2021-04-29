79b7137b248dfe5b2fc2877f233696800ea0c8c1
Alec Vance <alec.vance@gmail.com>
2021-04-29 11:46:21 -0500

    added optional max_rh value in config for each thermometer section; updated message legibility and sample config data

--- nest.py
+++ nest.py
@@ -52,7 +52,7 @@ smtp = config['DEFAULT']['smtp']
 smtp_port = config['DEFAULT']['smtp_port']
 
 # maximum relative humidity; greater than this will trigger an email if email is set.
-max_rh = int(config['DEFAULT']['max_rh'])
+default_max_rh = int(config['DEFAULT']['max_rh'])
 
 # cool to dry settings.
 cool_to_dry = bool(config['DEFAULT']['cool_to_dry'])
@@ -85,6 +85,18 @@ for deviceID, thermostat in thermostats.items():
     device_name_long = thermostat['name_long']
     filename = device_name_long + ".log"
 
+    max_rh = default_max_rh
+    if config.has_option(device_name_long,'max_rh'):
+        max_rh = int(config[device_name_long]['max_rh']) 
+        #print("max_rh for " + device_name_long + " is " + format(max_rh))
+
+        
+    if max_rh == 0:
+        print("config error: max_rh not defined / too low for ".device_name_long)
+        max_rh = default_max_rh
+
+
+
     print (device_name_long +":")
 
     #print(deviceID, 'corresponds to', device_name_long)
@@ -104,79 +116,80 @@ for deviceID, thermostat in thermostats.items():
     with open(filename, "a") as f:
         f.write('{}\t{}\t{}\t{}\t{}\t{}\n'.format(timeStr,ambient_temperature_f,humidity,hvac_state,target_temperature_f,fan_timer_active))
 
-    messageText = "Humidity at {} is currently at {}%RH and {}F.\n System status is {} and target temp is " .format(device_name_long,humidity,ambient_temperature_f,hvac_state)
+        messageText = "\n\r  Status: \t{}F \t{}%RH \tState: {} \n\r  Target: \t" .format(ambient_temperature_f,humidity, hvac_state)
 
     if(hvac_mode == "heat-cool"):
         target_temperature_f = thermostat['target_temperature_high_f']
         #target temp is a range
-        messageText += "{}F-{}F. \n ".format(thermostat['target_temperature_low_f'],target_temperature_f)
+        messageText += "{}F-{}F".format(thermostat['target_temperature_low_f'],target_temperature_f)
     else:
-        messageText += "{}F. \n".format(target_temperature_f)
+        messageText += "{}F".format(target_temperature_f)
+
+    messageText +=  " \t{}%RH \n\r".format(max_rh)
 
     #humidity alerts happen ONLY when the system is off
-    if (int(humidity) > max_rh) and ( hvac_state == "off"):
+    if (hvac_state == "off"):
 
-        messageText += ("# WARNING: Humidity at {} is above {}%RH. \n").format(device_name_long,max_rh)
+        if (int(humidity) > max_rh):
+        
 
-        #make sure cool to dry is on, and the current temp is greater than the minimum set in the config file
-        if cool_to_dry:
+            messageText += ("# WARNING: Humidity at {} is above {}%RH. \n").format(device_name_long,max_rh)
 
-            if ambient_temperature_f > cool_to_dry_min_f :
-                # turn on and lower temp on thermostat to 1 degree below target
-                if target_temperature_f >= ambient_temperature_f:
-                    target_temperature_f = ambient_temperature_f - 1;
+            #make sure cool to dry is on, and the current temp is greater than the minimum set in the config file
+            if cool_to_dry:
 
-                    post_json = {}
+                if ambient_temperature_f > cool_to_dry_min_f :
+                    # turn on and lower temp on thermostat to 1 degree below target
+                    if target_temperature_f >= ambient_temperature_f:
+                        target_temperature_f = ambient_temperature_f - 1;
 
-                    if(hvac_mode == "heat-cool"):
-                        if target_temperature_f >= thermostat['target_temperature_low_f']:
-                            post_json = {"target_temperature_high_f": target_temperature_f}
+                        post_json = {}
 
-                    else:
-                        if(hvac_mode == "cool"):
-                            post_json = {"target_temperature_f" : target_temperature_f}
+                        if(hvac_mode == "heat-cool"):
+                            if target_temperature_f >= thermostat['target_temperature_low_f']:
+                                post_json = {"target_temperature_high_f": target_temperature_f}
+
+                        else:
+                            if(hvac_mode == "cool"):
+                                post_json = {"target_temperature_f" : target_temperature_f}
 
 
-                    if(post_json != {}):
+                        if(post_json != {}):
 
-                        print(("setting new target temp: {}F").format(target_temperature_f))
+                            print(("setting new target temp: {}F").format(target_temperature_f))
 
-                        # post the new temperature
-                        deviceURL = "/devices/thermostats/" + deviceID
+                            # post the new temperature
+                            deviceURL = "/devices/thermostats/" + deviceID
 
-                        conn2 = http.client.HTTPSConnection("developer-api.nest.com")
+                            conn2 = http.client.HTTPSConnection("developer-api.nest.com")
 
-                        headers = {'Authorization': "Bearer {0}".format(token), 'Content-type': 'application/json'}
+                            headers = {'Authorization': "Bearer {0}".format(token), 'Content-type': 'application/json'}
 
-                        #print(json.dumps(post_json))
-                        conn2.request("PUT", deviceURL, json.dumps(post_json), headers=headers)
-                        response = conn2.getresponse()
-
-                        if response.status == 307:
-                            redirectLocation = urlparse(response.getheader("location"))
-                            conn2 = http.client.HTTPSConnection(redirectLocation.netloc)
+                            #print(json.dumps(post_json))
                             conn2.request("PUT", deviceURL, json.dumps(post_json), headers=headers)
-
                             response = conn2.getresponse()
-                            if response.status != 200:
-                                print(str(response.status) + response.read().decode() )
-                                raise Exception("Redirect with non 200 response")
 
-                        #messageText += str(response.code) + response.read().decode() + "\n"
-                        messageText += ("Lowering thermostat to {}F.\n").format(target_temperature_f)
+                            if response.status == 307:
+                                redirectLocation = urlparse(response.getheader("location"))
+                                conn2 = http.client.HTTPSConnection(redirectLocation.netloc)
+                                conn2.request("PUT", deviceURL, json.dumps(post_json), headers=headers)
+
+                                response = conn2.getresponse()
+                                if response.status != 200:
+                                    print(str(response.status) + response.read().decode() )
+                                    raise Exception("Redirect with non 200 response")
+
+                            #messageText += str(response.code) + response.read().decode() + "\n"
+                            messageText += ("Lowering thermostat to {}F.\n").format(target_temperature_f)
 
 
 
-            if(email):
-                email_alert(device_name_long, messageText)
-                print("sent email.")
+                if(email):
+                    email_alert(device_name_long, messageText)
+                    print("sent email.")
 
     #else:
          #messageText += "Humidity at {} is within range. ( <= {}%RH.) \n".format(device_name_long,max_rh)
-
-
-
-
 
 
     print(messageText)
